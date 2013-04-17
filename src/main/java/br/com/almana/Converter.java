9d7f847bd916fadea9a9b357ccd53df1e771b7e8
Francisco Portillo <francisco.portillo@gmail.com>
2013-04-16 21:49:46 -0300

    Reducing duplicates - yet work todo

--- src/main/java/br/com/almana/Converter.java
+++ src/main/java/br/com/almana/Converter.java
@@ -18,47 +18,77 @@ import org.codehaus.jettison.mapped.MappedNamespaceConvention;
 import org.codehaus.jettison.mapped.MappedXMLStreamReader;
 import org.codehaus.jettison.mapped.MappedXMLStreamWriter;
 
-
 public class Converter {
+
+	private StringReader stringReader;
+	private StringWriter stringWriter;
 	
+	private JAXBContext ctx;
+	private JSONObject jsonObject;
+
+	private Marshaller marshaller;
+	private Unmarshaller unmarshaller;
+
+	private Configuration config;
+	private MappedNamespaceConvention mappedNamespaceConvention;
+
+	private XMLStreamWriter xmlStreamWriter;
+	private XMLStreamReader xmlStreamReader;
+
 	public String toXml(Object object) throws JAXBException {
-		StringWriter stringWriter = new StringWriter();
-		JAXBContext ctx = JAXBContext.newInstance(object.getClass());
-		Marshaller marshaller = ctx.createMarshaller();
-		marshaller.marshal(object, stringWriter);
+		makeMarshaller(object);
+		marshal(object);
 		return stringWriter.toString();
 	}
 
+	private void makeMarshaller(Object object) throws JAXBException {
+		ctx = JAXBContext.newInstance(object.getClass());
+		marshaller = ctx.createMarshaller();
+	}
+
+	private void marshal(Object object) throws JAXBException {
+		stringWriter = new StringWriter();
+		marshaller.marshal(object, stringWriter);
+	}
+
 	public String toJson(Object object) throws JAXBException {
-		JAXBContext ctx = JAXBContext.newInstance(object.getClass());
-		Configuration config = new Configuration();
-		MappedNamespaceConvention mappedNamespaceConvention = new MappedNamespaceConvention(config);
-		StringWriter stringWriter = new StringWriter();
-		XMLStreamWriter xmlStreamWriter = new MappedXMLStreamWriter(mappedNamespaceConvention, stringWriter);
-		Marshaller marshaller = ctx.createMarshaller();
+		makeMarshaller(object);
+		setupForJson();
+		stringWriter = new StringWriter();
+		xmlStreamWriter = new MappedXMLStreamWriter(mappedNamespaceConvention,
+				stringWriter);
 		marshaller.marshal(object, xmlStreamWriter);
 		return stringWriter.toString();
 	}
 
+	private void setupForJson() {
+		config = new Configuration();
+		mappedNamespaceConvention = new MappedNamespaceConvention(config);
+	}
+
 	public <T> T xmlToObject(Class<T> clazz, String xml) throws JAXBException {
-		JAXBContext ctx = JAXBContext.newInstance(clazz);
-		Unmarshaller unmarshaller = ctx.createUnmarshaller();
-		StringReader stringReader = new StringReader(xml);
+		makeUnmarshaller(clazz);
+		stringReader = new StringReader(xml);
 		@SuppressWarnings("unchecked")
 		T t = (T) unmarshaller.unmarshal(stringReader);
 		return t;
 	}
 
-	public <T> T jsonToObject(Class<T> clazz, String json) throws JAXBException, JSONException, XMLStreamException {
-		JAXBContext ctx = JAXBContext.newInstance(clazz);
-		JSONObject jsonObject = new JSONObject(json);
-		Configuration config = new Configuration();
-		MappedNamespaceConvention mappedNamespaceConvention = new MappedNamespaceConvention(config);		
-		XMLStreamReader xmlStreamReader = new MappedXMLStreamReader(jsonObject, mappedNamespaceConvention);
-		Unmarshaller unmarshaller = ctx.createUnmarshaller();
+	public <T> T jsonToObject(Class<T> clazz, String json)
+			throws JAXBException, JSONException, XMLStreamException {
+		makeUnmarshaller(clazz);
+		jsonObject = new JSONObject(json);
+		setupForJson();
+		xmlStreamReader = new MappedXMLStreamReader(jsonObject,
+				mappedNamespaceConvention);
 		@SuppressWarnings("unchecked")
 		T t = (T) unmarshaller.unmarshal(xmlStreamReader);
 		return t;
+	}
+
+	private <T> void makeUnmarshaller(Class<T> clazz) throws JAXBException {
+		ctx = JAXBContext.newInstance(clazz);
+		unmarshaller = ctx.createUnmarshaller();
 	}
 
 }
